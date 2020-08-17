040f68327ee088e163b7b7f035e5caded0698632
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-08-17 21:55:44 +0200

    Mobile navigation implemented in user panel

--- src/pages/user/UserPanel.js
+++ src/pages/user/UserPanel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Route, Switch} from "react-router-dom";
 
 import Home from './Home';
@@ -8,11 +8,19 @@ import Restaurant from './Restaurant';
 import RestaurantList from './RestaurantList';
 import SignUp from "./SignUp";
 import Navbar from "../../components/Navbar";
+import MobileNav from "../../components/MobileNav";
+
 
 export default () => {
+    const [menuDisplayed, setMenuDisplay] = useState(false);
+    const toggleMenu = () => {
+        setMenuDisplay(!menuDisplayed);
+    };
+    const mobileNavigation = menuDisplayed? <MobileNav toggleMenu={toggleMenu}/> : '';
     return (
         <>
-            <Navbar/>
+            <Navbar toggleMenu={toggleMenu} menuDisplayed={menuDisplayed}/>
+            {mobileNavigation}
             <Switch>
                 <Route path='/' exact component={Home} />
                 <Route path='/profile' component={Profile} />
