3bb8b0e276939d904fb6c92e400e593d14234c15
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-08-17 21:55:16 +0200

    ToggleMenu button implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,17 @@ import React from 'react';
 import {Link, useHistory} from 'react-router-dom';
 import './Navbar.css';
 
-export default () => {
+export default (props) => {
     const currentRoute = useHistory().location.pathname.toLowerCase();
-    console.log(currentRoute);
     return (
         <nav className="navbar">
             <div className="navbar__logo">
                 <Link to={'/'}>WhereToEat</Link>
             </div>
             <div className="navbar__spacer"></div>
+            <button onClick={props.toggleMenu} className="navbar__menu-btn">
+                {props.menuDisplayed ? <i className="fas fa-times"></i> : <i className="fas fa-bars"></i>}
+            </button>
             <ul className="navbar__item-list">
                 <li className={currentRoute === '/' ? "navbar__item--active" : "navbar__item"}>
                     <Link to={'/'}>Find Restaurant</Link>
