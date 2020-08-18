aa368036f4b2b46b8da552c3251067c296d32215
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-08-18 15:36:46 +0200

    Login form implemented

--- src/pages/user/Login.js
+++ src/pages/user/Login.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React from 'react';
+import {Form} from 'react-final-form';
+import FormInput from "../../components/FormInput";
+import {Link} from "react-router-dom";
 
 export default () => {
+    const onSubmit = async values => {
+        await console.log(JSON.stringify(values));
+    };
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <Form
+            onSubmit={onSubmit}
+            render={({ handleSubmit, form, submitting, pristine, values }) => (
+                <form onSubmit={handleSubmit}>
+                    <FormInput name="email" type="email" label="Email" placeholder=""/>
+                    <FormInput name="password" type="password" label="Password" placeholder=""/>
+                    <div className="buttons">
+                        <button type="submit" disabled={submitting || pristine}>
+                            Submit
+                        </button>
+                    </div>
+                    No account yet? <Link to="/sign-up">Sign Up</Link>
+                    <pre>{JSON.stringify(values, 0, 2)}</pre>
+                </form>
+            )}/>
     );
 };
