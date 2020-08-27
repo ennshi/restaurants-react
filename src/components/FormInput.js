bde15a7c439715c8c5de1729568081d76edf0873
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-08-27 22:23:51 +0200

    FormInput created

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Field} from 'react-final-form';
+import './FormInput.css';
 
 export default (props) => {
     return (
-        <div>
-           <label>{props.label}</label>
-           <Field name={props.name} component="input" type={props.type} placeholder={props.placeholder}/>
+        <div className="input__container">
+           <label className="input__label">{props.label}</label>
+           <Field name={props.name} component="input" type={props.type} placeholder={props.placeholder} className={props.class}/>
         </div>
     )
 };
