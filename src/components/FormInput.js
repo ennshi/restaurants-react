28b55db8a547e4e6951b9e3db2a8612b1d1b88e1
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-09-02 14:36:50 +0200

    Error display implemented in FromInput

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -6,7 +6,17 @@ export default (props) => {
     return (
         <div className="input__container">
            <label className="input__label">{props.label}</label>
-           <Field name={props.name} component="input" type={props.type} placeholder={props.placeholder} className={props.class}/>
+           <Field
+               name={props.name}
+               validate={props.validate}
+           >
+               {({input, meta}) => (
+                   <>
+                        <input {...input} type={props.type} placeholder={props.placeholder} className={props.class} />
+                        {meta.error && meta.touched && <span className="input__error">{meta.error}</span>}
+                   </>
+               )}
+           </Field>
         </div>
     )
 };
