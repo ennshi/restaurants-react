d018e2a587f0b7ac8abfae222933e4b719ad5076
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-09-03 21:36:34 +0200

    SubmitHandler for Login

--- src/pages/user/Login.js
+++ src/pages/user/Login.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Form} from 'react-final-form';
 import FormInput from "../../components/FormInput";
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 import {required} from "../../helpers/formValidation";
+import fetchData from "../../helpers/fetchData";
 
 export default () => {
+    const [errors, setErrors] = useState(null);
+    const history = useHistory();
     const onSubmit = async values => {
-        await console.log(JSON.stringify(values));
+        const fetchedData = await fetchData('http://localhost:8080/auth/login', {
+            crossDomain: true,
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(values)
+        });
+        if(!fetchedData.error && !fetchedData.response.errors) {
+            localStorage.clear();
+            localStorage.setItem('token', fetchedData.response.token);
+            localStorage.setItem('user', fetchedData.response.userId);
+            localStorage.setItem('expiration', `${Date.now() + 3600*1000}`);
+            return history.push('/');
+        }
+        fetchedData.error ? setErrors(['Server error. Please try again later.']) : setErrors(Object.values(fetchedData.response.errors));
     };
     return (
         <div className="form__container form__container--dark">
@@ -16,6 +32,9 @@ export default () => {
                 const {handleSubmit, pristine, submitting, hasValidationErrors} = props;
                 const isDisabled = submitting || pristine || hasValidationErrors;
                 return (<form onSubmit={handleSubmit}>
+                    {errors ? <div className="form__error-block">
+                        {errors.map(error => <p className="form__error">{error}</p>)}
+                    </div> : ''}
                     <FormInput
                         name="email"
                         type="email"
