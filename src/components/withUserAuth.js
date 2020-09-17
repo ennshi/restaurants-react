3898c1b529ebf8ce892e14f4eae5210508fad2a7
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-09-17 16:08:57 +0200

    withUserAuth created

---
+++ src/components/withUserAuth.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { Redirect } from "react-router-dom";
+import { UserAuthContext } from "../contexts/UserAuth";
+
+const withUserAuth = ComposedComponent => {
+    return (props) => {
+        const { isLoggedIn } = useContext(UserAuthContext);
+        if(!isLoggedIn) {
+            return <Redirect to={'/login'} />;
+        }
+        return <ComposedComponent {...props} />
+    };
+};
+
+export default withUserAuth;
