98a8b1b31454290b1d8c28ab158703cf8a4de11b
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-09-18 16:59:52 +0200

    Fetch user data

--- src/pages/user/Profile.js
+++ src/pages/user/Profile.js
@@ -1,13 +1,39 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {Form} from 'react-final-form';
+import { useHistory } from 'react-router-dom';
 import withUserAuth from "../../components/withUserAuth";
 import {UserAuthContext} from "../../contexts/UserAuth";
 import FormInput from "../../components/FormInput";
 import ProfilePhoto from "../../components/ProfilePhoto";
+import fetchData from "../../helpers/fetchData";
 
 const Profile = (props) => {
-    const { credentials } = useContext(UserAuthContext);
+    const { credentials, handleLogout } = useContext(UserAuthContext);
     const [errors, setErrors] = useState(null);
+    const [userData, setUserData] = useState(null);
+    const history = useHistory();
+    useEffect(() => {
+        const fetchingData = async () => {
+            const fetchedData = await fetchData('http://localhost:8080/profile', {
+                crossDomain: true,
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${credentials.token}`,
+                    'Content-Type': 'application/json'
+                }
+            });
+            if (!fetchedData.errors.length) {
+                console.log(fetchedData.response);
+                return setUserData(fetchedData.response);
+            }
+            if(fetchedData.errors[0] === 'Authorization failed') {
+                handleLogout();
+                return history.push('/login', {errors: [fetchedData.errors[0]]});
+            }
+            setErrors(fetchedData.errors);
+        };
+        fetchingData();
+    }, []);
     const onSubmit = () => {
 
     };
