a14377457f65a01f468f2cb1cc877942e382674d
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-09-20 21:07:24 +0200

    UserReviewList created

---
+++ src/components/UserReviewList.js
@@ -0,0 +1,51 @@
+import React, {useContext, useEffect, useState} from 'react';
+import {useHistory} from 'react-router-dom';
+import Review from "./Review";
+import fetchData from "../helpers/fetchData";
+import {UserAuthContext} from "../contexts/UserAuth";
+import './UserReviewList.css';
+
+export default () => {
+    const [errors, setErrors] = useState(null);
+    const [reviews, setReviews] = useState(null);
+    const {credentials, handleLogout} = useContext(UserAuthContext);
+    const history = useHistory();
+    useEffect(() => {
+        const fetchingReviews = async () => {
+            const fetchedData = await fetchData(`http://localhost:8080/reviews?filter=creator::${credentials.userId}`, {
+                crossDomain: true,
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${credentials.token}`,
+                    'Content-Type': 'application/json'
+                }
+            });
+            if (!fetchedData.errors.length) {
+                return setReviews(fetchedData.response.reviews);
+            }
+            if(fetchedData.errors[0] === 'Authorization failed') {
+                handleLogout();
+                return history.push('/login', {errors: [fetchedData.errors[0]]});
+            }
+            setErrors(fetchedData.errors);
+        };
+        fetchingReviews();
+    }, []);
+    return (
+        <div className="user-review-list__container">
+            {errors ? <div className="form__error-block">
+                {errors.map((error, i) => <p className="form__error" key={i}>{error}</p>)}
+                </div> : ''
+            }
+            { reviews ?
+                <> { reviews.length ?
+                    <div>
+                        {reviews.map((review, i) => <Review type={'user'} review={review} key={i}/>)}
+                    </div> :
+                    <div>No Reviews</div>
+                }
+                </> : <p>Loading</p>
+            }
+        </div>
+    );
+};
--- src/pages/user/Profile.js
+++ src/pages/user/Profile.js
@@ -6,11 +6,13 @@ import {UserAuthContext} from "../../contexts/UserAuth";
 import FormInput from "../../components/FormInput";
 import ProfilePhoto from "../../components/ProfilePhoto";
 import fetchData from "../../helpers/fetchData";
+import UserReviewList from "../../components/UserReviewList";
 
 const Profile = (props) => {
     const { credentials, handleLogout } = useContext(UserAuthContext);
     const [errors, setErrors] = useState(null);
     const [userData, setUserData] = useState(null);
+    const [displayReviews, setDisplayReviews] = useState(false);
     const history = useHistory();
     useEffect(() => {
         const fetchingData = async () => {
@@ -72,66 +74,78 @@ const Profile = (props) => {
             history.push('/');
         }
     };
+    const toggleReviews = () => {
+        setDisplayReviews(!displayReviews);
+    };
     return (
         <>
         <header>Profile Info</header>
         <div className="form__container form__container--profile">
             {userData ?
                 <>
-                <ProfilePhoto url={userData.user.photoUrl}/>
-                <Form
-                    onSubmit={onSubmit}
-                    initialValues={{username: userData.user.username, email: userData.user.email}}
-                    render={(props) => {
-                        const {handleSubmit, pristine, submitting, hasValidationErrors} = props;
-                        const isDisabled = submitting || pristine || hasValidationErrors;
-                        return (
-                            <div className="form__body--light">
-                                <form onSubmit={handleSubmit}>
-                                    {errors ? <div className="form__error-block">
-                                        {errors.map(error => <p className="form__error">{error}</p>)}
-                                    </div> : ''}
-                                    <FormInput
-                                        name="username"
-                                        type="username"
-                                        label="Username"
-                                        placeholder=""
-                                        class="input--dark"
-                                        classLabel="input__label input__label--dark"
-                                    />
-                                    <FormInput
-                                        name="email"
-                                        type="email"
-                                        label="Email"
-                                        placeholder=""
-                                        class="input--dark"
-                                        classLabel="input__label input__label--dark"
-                                    />
-                                    <FormInput
-                                        name="password"
-                                        type="password"
-                                        label="Password"
-                                        placeholder=""
-                                        class="input--dark"
-                                        classLabel="input__label input__label--dark"
-                                    />
-                                    <FormInput
-                                        name="repeatedPassword"
-                                        type="password"
-                                        label="Repeat Password"
-                                        placeholder=""
-                                        class="input--dark"
-                                        classLabel="input__label input__label--dark"
-                                    />
-                                    <div className="btn__container">
-                                        <button type="submit" disabled={isDisabled} className={isDisabled ? "btn btn--inactive" : "btn btn--red"}>
-                                            Save Changes
+                <div className="profile-photo__container">
+                    <ProfilePhoto url={userData.user.photoUrl}/>
+                    <button className="btn btn--red" onClick={toggleReviews}>{displayReviews ? 'My Info' : `My Reviews (${userData.reviews.length})`}</button>
+                </div>
+                    { displayReviews ? <UserReviewList /> :
+                        <Form
+                            onSubmit={onSubmit}
+                            initialValues={{username: userData.user.username, email: userData.user.email}}
+                            render={(props) => {
+                                const {handleSubmit, pristine, submitting, hasValidationErrors} = props;
+                                const isDisabled = submitting || pristine || hasValidationErrors;
+                                return (
+                                    <div className="form__body--light">
+                                        <form onSubmit={handleSubmit}>
+                                            {errors ? <div className="form__error-block">
+                                                {errors.map((error, i) => <p className="form__error" key={i}>{error}</p>)}
+                                            </div> : ''}
+                                            <FormInput
+                                                name="username"
+                                                type="username"
+                                                label="Username"
+                                                placeholder=""
+                                                class="input--dark"
+                                                classLabel="input__label input__label--dark"
+                                            />
+                                            <FormInput
+                                                name="email"
+                                                type="email"
+                                                label="Email"
+                                                placeholder=""
+                                                class="input--dark"
+                                                classLabel="input__label input__label--dark"
+                                            />
+                                            <FormInput
+                                                name="password"
+                                                type="password"
+                                                label="Password"
+                                                placeholder=""
+                                                class="input--dark"
+                                                classLabel="input__label input__label--dark"
+                                            />
+                                            <FormInput
+                                                name="repeatedPassword"
+                                                type="password"
+                                                label="Repeat Password"
+                                                placeholder=""
+                                                class="input--dark"
+                                                classLabel="input__label input__label--dark"
+                                            />
+                                            <div className="btn__container">
+                                                <button type="submit" disabled={isDisabled}
+                                                        className={isDisabled ? "btn btn--inactive" : "btn btn--red"}>
+                                                    Save Changes
+                                                </button>
+                                            </div>
+                                        </form>
+                                        <button type="button" className="btn--link" onClick={onDeleteProfile}>Delete the
+                                            profile and related data
                                         </button>
-                                    </div>
-                                </form>
-                                <button type="button" className="btn--link" onClick={onDeleteProfile}>Delete the profile and related data</button>
-                            </div>);
-                    }}/>
+                                    </div>);
+                            }}
+                        />
+                    }
                     </>: ''}
         </div>
         </>
