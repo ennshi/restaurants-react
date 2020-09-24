6e9640d99350fafeffeaee91c57fbf4faaf246f4
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-09-24 22:58:49 +0200

    Revert "Feature/user reviews"

--- src/helpers/dateConverters.js
+++
@@ -1,8 +0,0 @@
-const strToDate = (dateStr) => (new Date(dateStr));
-
-const strToDDMMYYYY = (dateStr) => {
-    const dateObj = strToDate(dateStr);
-    return (`${dateObj.getDate()}/${dateObj.getMonth()}/${dateObj.getFullYear()}`);
-};
-
-module.exports = {strToDDMMYYYY, strToDate};
