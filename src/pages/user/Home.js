af97bece2e29cb1479b6b0880f3450f419792924
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-09-25 16:23:50 +0200

    RestaurantSearchForm implemented with searchHandler

--- src/pages/user/Home.js
+++ src/pages/user/Home.js
@@ -1,9 +1,22 @@
 import React from "react";
+import RestaurantSearchForm from "../../components/RestaurantSearchForm";
+import fetchData from "../../helpers/fetchData";
 
 export default () => {
+    const searchHandler = async (values) => {
+        const fetchedData = await fetchData(`http://localhost:8080/restaurant?filter=${values.filter}`, {
+            crossDomain: true,
+            method: 'GET',
+            headers: {
+                'Content-Type':'application/json'
+            }
+        });
+        console.log(fetchedData.response);
+    };
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
+        <>
+            <RestaurantSearchForm submitHandler={searchHandler}/>
+            <h1>Restaurant List</h1>
+        </>
     );
 };
