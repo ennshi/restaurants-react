2e7f606721591c6bda77a1248d7777c786a1af83
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-09-27 18:04:28 +0200

    Error displaying added in RestaurantSearchForm

--- src/components/RestaurantSearchForm.js
+++ src/components/RestaurantSearchForm.js
@@ -4,11 +4,11 @@ import FormInput from './FormInput';
 import {required} from '../helpers/formValidation';
 import './RestaurantSearchForm.css';
 
-export default ({submitHandler}) => {
+export default ({submitHandler, errors}) => {
     return (
         <div className="form__container restaurant-search-form__container">
             <header className="restaurant-search-form__header">
-                <h2>Loking for a place to have dinner?</h2>
+                <h2>Looking for a place to have dinner?</h2>
             </header>
             <Form
             onSubmit={submitHandler}
@@ -16,12 +16,16 @@ export default ({submitHandler}) => {
                 const {handleSubmit, pristine, submitting, hasValidationErrors} = props;
                 const isDisabled = submitting || pristine || hasValidationErrors;
                 return (<form onSubmit={handleSubmit}>
+                        {errors ? <div className="form__error-block">
+                            {errors.map((error, i) => <p className="form__error" key={i}>{error}</p>)}
+                        </div> : ''}
                         <FormInput
                             name="filter"
                             type="text"
                             placeholder="Where to?"
                             class="input--basic"
                             validate={required}
+                            hideError={true}
                             />
                         <div className="btn__container">
                             <button type="submit" disabled={isDisabled} className={isDisabled ? "btn btn--100 btn--inactive" : "btn btn--100 btn--red"}>
