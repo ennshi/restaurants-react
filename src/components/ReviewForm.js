20d1edbdce92fcab86c87845880cc3cf38774d04
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-10-03 23:05:03 +0200

    Review form updated, style added

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -4,20 +4,23 @@ import {useHistory} from 'react-router-dom';
 import {composeValidators, maxLength, minLength, required} from "../helpers/formValidation";
 import fetchData from "../helpers/fetchData";
 import {UserAuthContext} from "../contexts/UserAuth";
+import './ReviewForm.css';
 
-export default ({updateReview, onReset, review}) => {
+export default ({updateReview, addReview, onReset, review, restaurantId}) => {
     const {credentials, handleLogout} = useContext(UserAuthContext);
     const [errors, setErrors] = useState(null);
     const history = useHistory();
     const onSubmit = async (values) => {
-        const result = await fetchData(`http://localhost:8080/reviews/${review._id}`, {
+        const method = updateReview ? 'PUT' : 'POST';
+        const body = updateReview ? JSON.stringify({...values}) : JSON.stringify({...values, restaurant: restaurantId});
+        const result = await fetchData(`http://localhost:8080/reviews/${review ? review._id : ''}`, {
             crossDomain: true,
-            method: 'PUT',
             headers: {
                 'Authorization': `Bearer ${credentials.token}`,
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({...values})
+            method,
+            body
         });
         if (result.errors.length) {
             if (result.errors[0] === 'Authorization failed') {
@@ -26,26 +29,30 @@ export default ({updateReview, onReset, review}) => {
             }
             return setErrors(result.errors);
         }
-        updateReview({...review, ...values});
+        if(updateReview) {
+            return updateReview({...review, ...values});
+        }
+        addReview(result.response);
     };
     const options = Array(5).fill(1).map((v, i) => (<option value={i+1} key={i}>{i+1}</option>));
     return (
         <Form
-            initialValues={{text: review.text, rating: +review.rating}}
+            initialValues={review && {text: review.text, rating: +review.rating}}
             onSubmit={onSubmit}
             render={(props) => {
                 const {handleSubmit, pristine, submitting, hasValidationErrors} = props;
                 const isDisabled = submitting || pristine || hasValidationErrors;
-                return (<form onSubmit={handleSubmit}>
+                return (<form onSubmit={handleSubmit} className="review-form__container">
                     {errors ? <div className="form__error-block">
                         {errors.map((error, i) => <p className="form__error" key={i}>{error}</p>)}
                     </div> : ''}
                     <Field name="rating">
                         {({input, meta}) => (
-                            <>
-                                <select {...input} >{options}</select>
+                            <div className="review-form__select__container">
+                                <label className="review-form__select__label">Rating</label>
+                                <select {...input} className="review-form__select">{options}</select>
                                 {meta.error && meta.touched && <span className="input__error">{meta.error}</span>}
-                            </>
+                            </div>
                         )}
                     </Field>
                     <Field
@@ -54,7 +61,7 @@ export default ({updateReview, onReset, review}) => {
                     >
                         {({input, meta}) => (
                             <>
-                                <textarea {...input} className="review__text"></textarea>
+                                <textarea {...input} className="review-form__textarea"></textarea>
                                 {meta.error && meta.touched && <span className="input__error">{meta.error}</span>}
                             </>
                         )}
