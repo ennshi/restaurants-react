885b785e81535193d9e9ac3c2483d9caa79ef8af
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-10-11 00:07:49 +0200

    Header styling added

--- src/components/FeaturedRestaurants.js
+++ src/components/FeaturedRestaurants.js
@@ -4,13 +4,15 @@ import './FeaturedRestaurants.css';
 
 export default ({restaurants}) => {
     return (
-        <>
-        <header>
-            <h2>Featured This Month</h2>
-        </header>
-        <div className="featured-restaurants__container">
-            {restaurants.map((restaurant, i) => <FeaturedRestaurantCard restaurant={restaurant} key={i}/>)}
+        <div className="featured-restaurants__wrapper">
+            <header className="heading__container heading__container--accent heading__container--featured">
+                <div className="heading__wrapper--featured">
+                    <h1 className="heading">Featured This Month</h1>
+                </div>
+            </header>
+            <div className="featured-restaurants__container">
+                {restaurants.map((restaurant, i) => <FeaturedRestaurantCard restaurant={restaurant} key={i}/>)}
+            </div>
         </div>
-        </>
     );
 }
