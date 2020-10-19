cd9a74d7a7b05f58b8410fba984dd258aa1f9b4e
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-10-20 01:36:17 +0200

    ProfileForm errors display improved

--- src/components/ProfileForm.js
+++ src/components/ProfileForm.js
@@ -3,63 +3,69 @@ import FormInput from "./FormInput";
 import {Form} from "react-final-form";
 
 export default ({onSubmit, userData, errors, onDeleteProfile}) => {
-    return (
-        <Form
-            onSubmit={onSubmit}
-            initialValues={{username: userData.user.username, email: userData.user.email}}
-            render={(props) => {
-                const {handleSubmit, pristine, submitting, hasValidationErrors} = props;
-                const isDisabled = submitting || pristine || hasValidationErrors;
-                return (
-                    <div className="form__body--light">
-                        <form onSubmit={handleSubmit}>
-                            {errors ? <div className="form__error-block">
-                                {errors.map((error, i) => <p className="form__error" key={i}>{error}</p>)}
-                            </div> : ''}
-                            <FormInput
-                                name="username"
-                                type="username"
-                                label="Username"
-                                placeholder=""
-                                class="input--dark"
-                                classLabel="input__label input__label--dark"
-                            />
-                            <FormInput
-                                name="email"
-                                type="email"
-                                label="Email"
-                                placeholder=""
-                                class="input--dark"
-                                classLabel="input__label input__label--dark"
-                            />
-                            <FormInput
-                                name="password"
-                                type="password"
-                                label="Password"
-                                placeholder=""
-                                class="input--dark"
-                                classLabel="input__label input__label--dark"
-                            />
-                            <FormInput
-                                name="repeatedPassword"
-                                type="password"
-                                label="Repeat Password"
-                                placeholder=""
-                                class="input--dark"
-                                classLabel="input__label input__label--dark"
-                            />
-                            <div className="btn__container">
-                                <button type="submit" disabled={isDisabled}
-                                        className={isDisabled ? "btn btn--100 btn--inactive" : "btn btn--100 btn--red"}>
-                                    Save Changes
-                                </button>
-                            </div>
-                        </form>
-                        <button type="button" className="btn--link" onClick={onDeleteProfile}>Delete the
-                            profile and related data
-                        </button>
-                    </div>);
-            }}
-        />
+    return (userData ?
+            <Form
+                onSubmit={onSubmit}
+                initialValues={{username: userData.user.username, email: userData.user.email}}
+                render={(props) => {
+                    const {handleSubmit, pristine, submitting, hasValidationErrors} = props;
+                    const isDisabled = submitting || pristine || hasValidationErrors;
+                    return (
+                        <div className="form__body--light">
+                            <form onSubmit={handleSubmit}>
+                                {errors ? <div className="form__error-block">
+                                    {errors.map((error, i) => <p className="form__error" key={i}>{error}</p>)}
+                                </div> : ''}
+                                <FormInput
+                                    name="username"
+                                    type="username"
+                                    label="Username"
+                                    placeholder=""
+                                    class="input--dark"
+                                    classLabel="input__label input__label--dark"
+                                />
+                                <FormInput
+                                    name="email"
+                                    type="email"
+                                    label="Email"
+                                    placeholder=""
+                                    class="input--dark"
+                                    classLabel="input__label input__label--dark"
+                                />
+                                <FormInput
+                                    name="password"
+                                    type="password"
+                                    label="Password"
+                                    placeholder=""
+                                    class="input--dark"
+                                    classLabel="input__label input__label--dark"
+                                />
+                                <FormInput
+                                    name="repeatedPassword"
+                                    type="password"
+                                    label="Repeat Password"
+                                    placeholder=""
+                                    class="input--dark"
+                                    classLabel="input__label input__label--dark"
+                                />
+                                <div className="btn__container">
+                                    <button type="submit" disabled={isDisabled}
+                                            className={isDisabled ? "btn btn--100 btn--inactive" : "btn btn--100 btn--red"}>
+                                        Save Changes
+                                    </button>
+                                </div>
+                            </form>
+                            <button type="button" className="btn--link" onClick={onDeleteProfile}>Delete the
+                                profile and related data
+                            </button>
+                        </div>);
+                }}
+            /> :
+            (errors ?
+                <div className="form__error-block">
+                    {errors.map((error, i) => <p className="form__error" key={i}>{error}</p>)}
+                </div> :
+                null
+            )
     );
 }
