b4940970b16a66d97638e42ce58b9e4cf1f88667
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-10-20 16:11:49 +0200

    imgLoader added into FeaturedRestaurantCard

--- src/components/FeaturedRestaurantCard.js
+++ src/components/FeaturedRestaurantCard.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './FeaturedRestaurantCard.css';
 import {convertUrl} from "../helpers/pathConverters";
 import {Link} from "react-router-dom";
+import Skeleton from "react-loading-skeleton";
 
 export default ({restaurant}) => {
+    const [imgLoaded, setImgLoaded] = useState(false);
+    const isImgLoaded = () => {
+        setImgLoaded(true);
+    };
     return (
         <div className="featured-restaurant-card__container">
             <Link to={`restaurant/${restaurant._id}`}>
-                <img src={convertUrl(restaurant.photoUrl)} className="featured-restaurant-card__photo" alt={restaurant.name}/>
+                {!imgLoaded &&
+                <div className="featured-restaurant-card__photo" style={{margin: '0 auto'}}>
+                    <Skeleton height="16rem"/>
+                </div>
+                }
+                <img src={convertUrl(restaurant.photoUrl)} className={imgLoaded ? 'featured-restaurant-card__photo' : 'featured-restaurant-card__photo hidden'} alt={restaurant.name} onLoad={isImgLoaded}/>
                 <span className="featured-restaurant-card__rating"><i className="fas fa-asterisk"></i>{restaurant.avgRating ? `${restaurant.avgRating.toFixed(1)}/5` : '-/5'}</span>
                 <h4 className="featured-restaurant-card__header">{restaurant.name}</h4>
                 <p className="featured-restaurant-card__country">{restaurant.location.formattedAddress.split(',').slice(-1)}</p>
