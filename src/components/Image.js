39fe5a5a06a601f240a4b14de04325f78efb6e2f
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-10-21 00:21:10 +0200

    Image created (with loader implementation)

---
+++ src/components/Image.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import Skeleton from "react-loading-skeleton";
+
+export default ({classes, alt, url, width, height}) => {
+    const [imgLoaded, setImgLoaded] = useState(false);
+    const isLoaded = () => {
+        setImgLoaded(true);
+    };
+    return (
+        <>
+            {!imgLoaded && <Skeleton width={width} height={height} />}
+            <img src={url} onLoad={isLoaded} alt={alt} className={imgLoaded ? classes : `${classes} hidden`}/>
+        </>
+    )
+};
