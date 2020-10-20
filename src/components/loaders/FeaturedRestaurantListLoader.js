8badd8d499b5fff163587dbf3c91a1930a331a65
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-10-21 00:33:53 +0200

    Style of FeaturedRestaurantListLoader improved

--- src/components/loaders/FeaturedRestaurantListLoader.js
+++ src/components/loaders/FeaturedRestaurantListLoader.js
@@ -5,9 +5,11 @@ import Skeleton from "react-loading-skeleton";
 export default () => {
     return (Array(4).fill(1).map((el, i) => (
         <div className="featured-restaurant-card__container" key={i}>
-            <div style={{padding: '1rem 1rem 2rem 1rem'}}>
-                <Skeleton height="16rem"/>
-                <Skeleton height="1.8rem"/>
+            <div style={{padding: '0 1rem 1rem 1rem'}}>
+                <Skeleton height="16rem" width="12rem"/>
+                <div className="featured-restaurant-card__header">
+                    <Skeleton height="1.8rem" width="12rem"/>
+                </div>
             </div>
         </div>
         )
