89efb876dcbeaa5c26f5ed7ead40bd09581b87eb
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-10-21 00:37:35 +0200

    Image implemented in RestaurantCard

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './RestaurantCard.css';
 import {convertUrl} from "../helpers/pathConverters";
 import {Link} from 'react-router-dom';
+import Image from "./Image";
 
 export default ({restaurant}) => {
     return (
         <div className="restaurant-card__container">
             <Link to={`restaurant/${restaurant._id}`}>
-                <img src={convertUrl(restaurant.photoUrl)} className="restaurant-card__photo" />
+                <Image alt={restaurant.name} url={convertUrl(restaurant.photoUrl)} classes="restaurant-card__photo"/>
                 <h4 className="restaurant-card__header">{restaurant.name}</h4>
                 <p className="restaurant-card__text">{restaurant.location.formattedAddress}</p>
                 <div className="restaurant-card__rating-block">
