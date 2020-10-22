edbb5233389d9059c54604916ec94bfc17b71cb5
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-10-23 01:17:35 +0200

    FeaturedRestaurantCard link with target blank

--- src/components/FeaturedRestaurantCard.js
+++ src/components/FeaturedRestaurantCard.js
@@ -7,7 +7,7 @@ import Image from "./Image";
 export default ({restaurant}) => {
     return (
         <div className="featured-restaurant-card__container">
-            <Link to={`restaurant/${restaurant._id}`}>
+            <Link to={`restaurant/${restaurant._id}`} target="_blank" rel="noreferrer noopener">
                 <Image height="16rem" url={convertUrl(restaurant.photoUrl)} alt={restaurant.name} classes="featured-restaurant-card__photo" width="12rem" />
                 <span className="featured-restaurant-card__rating"><i className="fas fa-asterisk"></i>{restaurant.avgRating ? `${restaurant.avgRating.toFixed(1)}/5` : '-/5'}</span>
                 <h4 className="featured-restaurant-card__header">{restaurant.name}</h4>
