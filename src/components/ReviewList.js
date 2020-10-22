f66bbece1b22f37defce2db42306527427291c29
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-10-23 00:14:45 +0200

    onDeleteReview changes totalNumber of reviews

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Review from "./Review";
 import './ReviewList.css';
 
-export default ({type, reviews, errors, setReviews}) => {
+export default ({type, reviews, errors, setReviews, totalNumber, setTotalNumber}) => {
     const onDeleteReview = (id) => {
         setReviews(reviews.filter(review => review._id !== id));
+        setTotalNumber(prev => prev - 1);
     };
     return (
         <>
@@ -15,6 +16,7 @@ export default ({type, reviews, errors, setReviews}) => {
             { reviews ?
                 (reviews.length ?
                 <div className="review-list__body">
+                    <span className="text--small-blue" style={{width: '100%', textAlign: 'right', margin: "0 3rem 1rem 0"}}>{totalNumber} reviews</span>
                     {reviews.map((review) => <Review type={type} reviewData={review} key={review._id} onDeleteReview={onDeleteReview}/>)}
                 </div> :
                 <div>No Reviews</div>) :
