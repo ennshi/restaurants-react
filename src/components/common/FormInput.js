ffc35c1ea757451bf7a65bc831a33ace7f99df29
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-11-03 12:52:51 +0100

    Labels for form input improved

--- src/components/common/FormInput.js
+++ src/components/common/FormInput.js
@@ -5,14 +5,14 @@ import './FormInput.css';
 export default (props) => {
     return (
         <div className="input__container">
-           <label className={props.classLabel || "input__label"}>{props.label}</label>
+           <label className={props.classLabel || "input__label"} htmlFor={props.name}>{props.label}</label>
            <Field
                name={props.name}
                validate={props.validate}
            >
                {({input, meta}) => (
                    <>
-                        <input {...input} type={props.type} placeholder={props.placeholder} className={props.class} />
+                        <input {...input} type={props.type} placeholder={props.placeholder} className={props.class} id={props.name} />
                         {props.hideError ? '' : meta.error && meta.touched && <span className="input__error">{meta.error}</span>}
                    </>
                )}
