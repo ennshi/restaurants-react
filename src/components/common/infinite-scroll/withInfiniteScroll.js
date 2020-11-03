b3ff9f6f40ae78bf813a1c6b945ccff459e29d19
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-11-03 21:35:17 +0100

    withInfiniteScroll refactored

--- src/components/common/infinite-scroll/withInfiniteScroll.js
+++ src/components/common/infinite-scroll/withInfiniteScroll.js
@@ -1,13 +1,30 @@
 import React, {useEffect, useRef, useState} from 'react';
+import fetchData from "../../../helpers/fetchData";
 
 export const withInfiniteScroll = WrappedComponent => {
     return (props) => {
         const [items, setItems] = useState(null);
+        const [itemErrors, setItemErrors] = useState(null);
         const page = useRef(1);
         const [nextItems, setNextItems] = useState(true);
         const [totalNumber, setTotalNumber] = useState(null);
         const isFetching = useRef(false);
-        
+        const fetchItems = async (url, itemsType) => {
+            isFetching.current = true;
+            const fetchedData = await fetchData({
+                url: `${url}&page=${page.current}`,
+                method: 'GET'
+            });
+            isFetching.current = false;
+            if (!fetchedData.errors.length) {
+                page.current++;
+                !totalNumber && setTotalNumber(fetchedData.response.totalNumber);
+                return setItems(prevVal => prevVal ?
+                    [...prevVal, ...fetchedData.response[itemsType]] :
+                    fetchedData.response[itemsType]);
+            }
+            setItemErrors(fetchedData.errors);
+        };
         useEffect(() => {
             items && setNextItems(totalNumber > items.length);
         }, [items]);
@@ -21,6 +38,8 @@ export const withInfiniteScroll = WrappedComponent => {
                 totalNumber={totalNumber}
                 setTotalNumber={setTotalNumber}
                 isFetching={isFetching}
+                itemErrors={itemErrors}
+                fetchItems={fetchItems}
             />;
     }
 };
